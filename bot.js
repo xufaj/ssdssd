f17ead281b810567f8f91a3a190bfef0d5ebecad
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-02 13:19:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -780,44 +780,5 @@ message.author.send(`
 ─════════════ {✯ By SAJED...👻#8844 } ════════════─
 **
 `);
-}
-})
- const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "$";
-  var adminprefix = "&"
-const developers = ["518078136139644955"]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-     
-  if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`SAJED   ${argresult}**`)
-  } else
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`SAJED   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`SAJED   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/One");
-      message.channel.send(`SAJED`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
- 
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
